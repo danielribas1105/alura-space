613a76ecd4937c4e309de9ae25852be1eb6a59b8
daniel.ribas <daniel.ribas@isdbflowtech.com>
2025-10-17 10:29:18 -0300

    update project

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from gallery.views import index, image
+from gallery.views import buscar, index, image
 
 urlpatterns = [
    path('', index, name='index'),
-   path('image/<int:foto_id>', image, name='image')
+   path('image/<int:foto_id>', image, name='image'),
+   path('buscar', buscar, name='buscar')
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -12,3 +12,13 @@ def index(request):
 def image(request, foto_id):
    fotografia = get_object_or_404(Fotografia, pk=foto_id)
    return render(request, "gallery/image.html", {"fotografia": fotografia})
+
+def buscar(request):
+   fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
+
+   if "buscar" in request.GET:
+      nome_buscar = request.GET['buscar']
+      if nome_buscar:
+         fotografias = fotografias.filter(nome__icontains=nome_buscar)
+
+   return render(request, "gallery/buscar.html", {"cards": fotografias})
